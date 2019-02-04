1fb798d6a4b973167a1e62593d21fd60def10c13
Kevin Rodríguez <_@kevinrodriguez.io>
2019-02-04 17:08:03 -0600

    Create SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+
+  state = { term: '' };
+  searchIconRef = React.createRef();
+
+  constructor(props) {
+    super(props);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
+  onInputChanged = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <h1>Unsplash image search</h1>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <div className={`ui icon input ${this.props.isSearching ? 'loading' : ''}`}>
+              <input
+                type="text"
+                placeholder="Try typing a search term and hit enter"
+                type="text"
+                value={this.state.term}
+                onChange={this.onInputChanged}
+              />
+              <i onClick={this.onFormSubmit} className="inverted circular search link icon"></i>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
